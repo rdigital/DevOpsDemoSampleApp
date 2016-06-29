4fec511f3746946dfb28505c67423edf4f156400
dluser <tushar.madiya@capgemini.com>
2016-06-29 10:36:19 +0000

    Modified the Route path

--- DevOpsDemoSampleApp/App_Start/RouteConfig.cs
+++ DevOpsDemoSampleApp/App_Start/RouteConfig.cs
@@ -14,9 +14,10 @@ namespace DevOpsDemoSampleApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}", 
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },  //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "DevOpsDemoSampleApp.Controllers", }
             );
         }
     }
